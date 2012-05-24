9d8e8db9f1d2bd52bf9ce6e24e46bf43ab942649
Jeffrey Ness <jeffrey.ness@rackspace.com>
2012-05-24 16:55:14 -0500

    example of copying configuration

--- BenderBot/Configuration.py
+++ BenderBot/Configuration.py
@@ -2,8 +2,15 @@ from ConfigParser import ConfigParser
 import os
 
 def get_config():
-    '''returns the configuration found in cfg_name,
-    or raises an Exception'''
+    '''This method will check the ``cfg_name`` for a BenderBot
+    configuration file.
+    
+    To get started copy the ``config/bender.conf-example`` to
+    ``~/.bender.conf``::
+        
+        cp config/bender.conf-example ~/.bender.conf
+    
+    '''
     cfg_name = '~/.bender.conf'
     cfg = os.path.expanduser(cfg_name)
     if os.path.exists(cfg):
