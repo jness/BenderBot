c36424307449d59e2156c2d32b3a5a6ff5267d14
Jeffrey Ness <jeffrey.ness@rackspace.com>
2012-12-13 11:50:44 -0600

    try to keep the bot alive by restarting after it crashes

--- BenderBot/Bender.py
+++ BenderBot/Bender.py
@@ -43,37 +43,59 @@ def main():
     
     # Connect to IRC
     cfg = dict(config.items('IRC'))
-    irc = IRC(logger=logger, queue=queue, **cfg)
-    irc.connect()
-    irc.joinchannel()
+    
+    # Catch exceptions to restart Bot if dead
+    wait = 60
+    while True:
+        try:
+            irc = IRC(logger=logger, queue=queue, **cfg)
+            irc.connect()
+            irc.joinchannel()
 
-    # Start the IRC read process that handles PING/PONG,
-    # and adding any IRC messages to RabbitMQ
-    logger.info('Starting IRCProcess Read')
-    
-    irc_read = IRCProcess(name='irc_read', target='read',
-                          logger=logger, config=config, queue=queue, irc=irc)
-    irc_read.daemon = True
-    irc_read.start()
-    
-    # Start the IRC write process
-    logger.info('Starting IRCProcess Write')
-    irc_write = IRCProcess(name='irc_write', target='write',
-                           logger=logger, config=config, queue=queue, irc=irc)
-    irc_write.daemon = True
-    irc_write.start()
-    
-    # keep an eye on our IRC Read and Write processes
-    while irc_write.is_alive() and irc_read.is_alive():
-        # take a nap ;)
-        sleep(1)
+            # Start the IRC read process that handles PING/PONG,
+            # and adding any IRC messages to RabbitMQ
+            logger.info('Starting IRCProcess Read')
 
-    # the only way we end up here is if irc_write or irc_read died,
-    # lets report the status of each and shutdown
-    write, read = (irc_write.is_alive(), irc_read.is_alive())
-    logger.warn('IRC Write process alive: %s' % write)
-    logger.warn('IRC Read process alive: %s' % read)
-    logger.info('BenderBot is shutting down')
+            irc_read = IRCProcess(name='irc_read', target='read',
+                                  logger=logger, config=config, queue=queue, irc=irc)
+            irc_read.daemon = True
+            irc_read.start()
+
+            # Start the IRC write process
+            logger.info('Starting IRCProcess Write')
+            irc_write = IRCProcess(name='irc_write', target='write',
+                                   logger=logger, config=config, queue=queue, irc=irc)
+            irc_write.daemon = True
+            irc_write.start()
+
+            # Keep the Bot alive as long as our IRC Read/Write
+            # process are alive
+            while irc_write.is_alive() and irc_read.is_alive():
+                # take a nap ;)
+                sleep(1)
+
+            # the only way we end up here is if irc_write or irc_read died,
+            # lets report the status of each and raise an exception
+            write, read = (irc_write.is_alive(), irc_read.is_alive())
+            if not write:
+                logger.error('IRC Write process alive: %s' % write)
+                if read:
+                    logger.warn('Shutting down IRC Read process')
+                    irc_read.terminate()
+            if not read:
+                logger.error('IRC Read process alive: %s' % read)
+                if write:
+                    logger.warn('Shutting down IRC Write process')
+                    irc_write.terminate()
+            # Finally raise an Exception
+            raise Exception('BenderBot shutting down')
+
+        # Capture any exception and sleep for our wait time
+        # before attempt to restart the bot
+        except Exception, e:
+            logger.error(e)
+            logger.warn('Waiting %s seconds before restarting...' % wait)
+            sleep(wait)
 
 if __name__ == '__main__':
     main()
